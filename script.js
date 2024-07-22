eedd9f1598ca53971ed59f4f30d863b53796c0cd
jeanneps <jeanepacchecosoares@gmail.com>
2024-07-22 08:54:14 -0300

    atividade logica e js

---
+++ script.js
@@ -0,0 +1,15 @@
+// 1- //
+var minhaVariavel = óla, mundo;
+console.log(minhaVariavel);
+
+//2//
+var meuarray = ["jaca", "melancia", "pera", "pessego"];
+meuarray.forEach(meuarray => {
+    console.log(meuarray);
+});
+//3//
+var frutas = ["maça", "banana", "laranja", "uva"];
+var  terceiro = frutas[2];
+console.log("Terceiro elemento do array:", terceiro);
+//4//
+
