36488e779bebcd1a4aa0fcc4a0b0921a97a7b7bf
jeanneps <jeanepacchecosoares@gmail.com>
2024-07-23 21:26:49 -0300

    atividade logica e js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var minhaVariavel = óla, mundo;
 console.log(minhaVariavel);
 
 //2//
-var meuarray = ["jaca", "melancia", "pera", "pessego"];
+const meuarray = ["jaca", "melancia", "pera", "pessego"];
 meuarray.forEach(meuarray => {
     console.log(meuarray);
 });
@@ -12,4 +12,24 @@ var frutas = ["maça", "banana", "laranja", "uva"];
 var  terceiro = frutas[2];
 console.log("Terceiro elemento do array:", terceiro);
 //4//
+//11//
+var numero = 5
+if(numero ===5){
+    console.log ("o Número é igual a 5.");
+} else if (numero == 5) {
+    console.log ("O número é igual a 5, com conversão de tipo.");
 
+}else {
+    console.log ("O numero não é igual a 5.");
+}
+//12//
+const numeros = [1,2,3,,4,5,6,7,8,9,10]
+for (pares)
+    console.log(numeros,(pares))
+
+//13//
+function (nota1,nota2,nota3)
+for(nota1+nota2+nota3)
+
+    //14//
+    
